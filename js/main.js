f66f102bbcc86152ab43a8d60c44d2edbed0c31b
SorenWalljasper <swalljasper@wisc.edu>
2017-01-09 19:45:59 -0600

    add map elements

--- js/main.js
+++ js/main.js
@@ -1,435 +1,288 @@
-//load tileset into map
+dataKey = ['Murders 2008','Murders 2009','Murders 2010','Murders 2011','Murders 2012','Murders 2013','Murders 2014']
 
-var policeLayer;
-var HospitalsLayer
+year = ['2008','2009','2010','2011','2012','2013','2014']
 
-function createMap(){
-	var map = L.map('map', {
-		center: [41.8058,-87.5935],
-		zoom: 12
-	});
-
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	maxZoom: 19,
-	minZoom: 12,
-	id: 'swal94.4103e88e',
-	accessToken: 'pk.eyJ1Ijoic3dhbDk0IiwiYSI6ImNpZnk5aWdzcDR5dDl0ZWx5dDhwZW13ejAifQ.y18LYK4VbBo8evRHtqiEiw'
-}).addTo(map);
-
-getData(map);
-getMoreData(map);
-getEvenMoreData(map);
+x = 0
+a = 0
 
 
-}
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3dhbDk0IiwiYSI6ImNpZnk5aWdzcDR5dDl0ZWx5dDhwZW13ejAifQ.y18LYK4VbBo8evRHtqiEiw';
+var tileset = 'mapbox.streets';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: {
+        "version": 8,
+        "sources": {
+            "raster-tiles": {
+                "type": "raster",
+                "url": "mapbox://swal94.4103e88e",
+                "tileSize": 256
+            }
+        },
+        "layers": [{
+            "id": "swal94.4103e88e",
+            "type": "raster",
+            "source": "raster-tiles",
+            "minzoom": 11,
+            "maxzoom": 12
+        }]
+    },
+    center: [-87.5935,41.805], // starting position
+    zoom: 11,
+    interactive: false
+});
 
+map.on('load', function(){
+    getData(map)
+    getPoliceData(map)
+    getHospitalData(map)
 
-
-//load data to map
+})
 
 function getData(map) {
-		$.ajax("../data/southSide.geojson", {
-		dataType: "json",
-		success: function(data){
+        $.ajax("../data/southSide.geojson", {
+        dataType: "json",
+        success: function(data){
 
-			var attributes = processData(data);
+            
+            map.addSource("points", {
+                "type": "geojson",
+                "data": data
+            });
 
-			createPropSymbols(data, map, attributes);
-			createSequenceControls(map, attributes);
-			createLegend(map, attributes);
+            
 
-		}
-	});
-}	
-
-
-
-
-
-
-//make proportional symbols change sizes based on selected years
-
-function calcPropRadius(attValue) {
-
-	var scaleFactor = 150;
-	var area = attValue * scaleFactor;
-	var radius = Math.sqrt(area/Math.PI);
-
-	return radius;
-};
-
-
-
-
-function pointToLayer(feature, latlng, attributes) {
-
-	var attribute = attributes[1];
-
-	var options = {
-		fillColor: "#fff",
-		color: "#fff",
-		weight: 1,
-		opacity: 1,
-		fillOpacity: 0.8
-	};
-
-	var attValue = Number(feature.properties[attribute]);
-
-	options.radius = calcPropRadius(attValue);
-
-	var layer = L.circleMarker(latlng, options);
-
-	var popup = new Popup(feature.properties, attribute, layer, options.radius);
-
-	popup.bindToLayer();
-
-	layer.on({
-		mouseover: function(){
-			this.openPopup();
-		},
-		mouseout: function(){
-			this.closePopup();
-		},
-		click: function(){
-			$("#popup").html(popupContent);
-			console.log(popupContent);
-		}
-	});
-
-	return layer;
-};
-
-function createPropSymbols(data, map, attributes){
-
-	L.geoJson(data, {
-		pointToLayer: function(feature, latlng){
-			return pointToLayer(feature, latlng, attributes);
-		}
-	}).addTo(map);
-};
-
-//create sequence controls
-
-function createSequenceControls(map, attributes){
-
-
-	$('#panel').append('<input class="range-slider" type="range">')
-
-	
-	$('#panel').append('<button class="skip" id="reverse">reverse</button>');
-    $('#panel').append('<button class="skip" id="forward">forward</button>');
-    $('#reverse').html('<img src="../images/reverse-arrow.png">');
-    $('#forward').html('<img src="../images/forward-arrow.png">');
-
-    $('.range-slider').on('input',function(){
-		console.log("yo")
-
-		var index = $(this).val();
-
-		updatePropSymbols(map, attributes[index]);
-		
-	});
-
-	$('.skip').click(function(){
-		var index = $('.range-slider').val();
-
-		if($(this).attr('id')== 'forward'){
-			index++;
-			index = index > 6 ? 0 : index;
-		} else if($(this).attr('id')== 'reverse'){
-			index--;
-			index = index < 0 ? 6 : index;
-		};
-
-		$('.range-slider').val(index);
-
-		$('.range-slider').attr({
-			max: 6,
-			min: 0,
-			value: 0,
-			step: 1
-		});
-
-		updatePropSymbols(map, attributes[index]);
-	});
-};
-
-//processing the data
-
-function processData(data){
-	var attributes = [];
-	var properties = data.features[0].properties;
-
-	for (var attribute in properties){
-		if (attribute.indexOf("Murders") > -1){
-			attributes.push(attribute);
-		};
-	};
-
-	return attributes;
-};
-
-function updatePropSymbols(map, attribute){
-	console.log(attribute)
-	map.eachLayer(function(layer){
-		if (layer.feature && layer.feature.properties[attribute]){
-			var props = layer.feature.properties;
-
-			var radius = calcPropRadius(props[attribute]);
-			layer.setRadius(radius);
-
-			var popup = new Popup(props, attribute, layer, radius);
-
-			popup.bindToLayer();
-
-		};
-	});
-
-	updateLegend(map, attribute);
+            createPoints(data);
+            slide(data)
+            popup(map, data)
+            
+        }
+    });
 }
 
-function Popup(properties, attribute, layer, radius){
-    this.properties = properties;
-    this.attribute = attribute;
-    this.layer = layer;
-    this.year = attribute.split(" ")[1];
-    this.murders = this.properties[attribute];
-    this.content = "<p><b>Neighborhood:</b> " + this.properties.Neighborhood + "</p><p><b>Murders in " + this.year + ":</b> " + this.murders + "</p>";
 
-    this.bindToLayer = function(){
-        this.layer.bindPopup(this.content, {
-            offset: new L.Point(0,-radius)
+function popup(map, data){
+    
+    var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+
+var idKey = ['murders-default','murders-changed']
+
+map.on('mousemove', function(e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: [idKey[a]] });
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+
+    if (!features.length) {
+        popup.remove();
+        return;
+    }
+
+    var feature = features[0];
+
+    var oneDataKey = dataKey[x]
+
+    
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(feature.geometry.coordinates)
+        .setHTML(feature.properties.Neighborhood + ": " + feature.properties[oneDataKey] + " murders")
+        .addTo(map);
+});
+}
+
+function slide(data, points) {
+
+    $("#year").append($("<h2>" + year[x] + "</h2>"))
+
+    document.getElementById('slider').addEventListener('input', function(e) {
+            //var year = parseInt(e.target.value, 6);
+
+            updatePoints(data, e.target.value)
+            // addPopup(data, e.target.value, points)
+
+            $("#year").remove()
+            $(".map-overlay-inner").append($("<label id='year'></label>"))
+            $("#year").append($("<h2>" + year[e.target.value] + "</h2>"))
+
+            x = e.target.value
+           
         });
-    };
 };
 
-function createLegend(map, attributes){
-	var LegendControl = L.Control.extend({
-		options: {
-			position: 'topright'
-		},
+function createPoints(data){
+    
+    map.addLayer({
+                'id': 'murders-default',
+                'type': 'circle',
+                'source': 'points',
+                'layout': {
+                 'visibility': 'visible'
+             },
+             'paint': {
+                    'circle-radius': {
+                        property: dataKey[x],
+                        stops: [
+                            [0, 5],
+                            [5, 10],
+                            [10, 15],
+                            [15, 20],
+                            [20, 25],
+                            [25, 30],
+                            [30, 35],
+                            [35, 40],
+                            [40, 45]
+                        ]
+                    },
 
-		onAdd: function (map) {
-			var container = L.DomUtil.create('div', 'legend-control-container');
-			$(container).append('<div id="temporal-legend">')
+                    'circle-color': 'rgba(255,255,255,0.8)'
+                },
+                'source-layer': 'museum-cusco'
+            })
 
-			var svg = '<svg id="attribute-legend" width="160px" height="60px">';
+}
 
-			var circles = {
-				max: 20,
-				mean: 40,
-				min: 60
-			};
+function updatePoints(data, y){
 
-			for (var circle in circles){
+    if (a == 0) {
+        map.removeLayer('murders-default')
+    } else if (a == 1) {
+        map.removeLayer('murders-changed')
+    }
+    
+    map.addLayer({
+        'id': 'murders-changed',
+        'type': 'circle',
+        'source': 'points',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'paint': {
+            'circle-radius': {
+                property: dataKey[y],
+                stops: [
+                    [0, 5],
+                    [5, 10],
+                    [10, 15],
+                    [15, 20],
+                    [20, 25],
+                    [25, 30],
+                    [30, 35],
+                    [35, 40],
+                    [40, 45]
+                ]
+            },
 
-				svg += '<circle class="legend-circle" id="' + circle + '" fill="#fff" fill-opacity="0.8" stroke = "#fff" cx="30"/>';
-				svg += '<text id="' + circle + '-text" x="65" y="' + circles[circle] + '"></text>';
-			};
+            'circle-color': 'rgba(255,255,255,0.8)'
+        },
+        'source-layer': 'museum-cusco'
+    });
 
-			svg += "</svg>";
+    a = 1
 
-			$(container).append(svg);
-
-			return container;
-		}
-	});
-
-	map.addControl(new LegendControl());
-
-	updateLegend(map, attributes[0]);
-};
-
-function getCircleValues(map, attribute){
-	var min = Infinity,
-		max = -Infinity;
-
-	map.eachLayer(function(layer){
-		if (layer.feature){
-			var attributeValue = Number(layer.feature.properties[attribute]);
-
-			if (attributeValue < min){
-				min = attributeValue;
-			};
-
-			if (attributeValue > max){
-				max = attributeValue;
-			};
-		};
-	});
-
-	var mean = (max + min)/2;
-
-	return{
-		max: max,
-		mean: mean,
-		min: min,
-	};
-};
-
-function updateLegend(map, attribute){
-	var year = attribute.split(" ")[1];
-	var content = "Murders in " + year;
-
-	$('#temporal-legend').html(content);
-
-	var circleValues = getCircleValues(map, attribute);
-
-	for (var key in circleValues){
-
-		var radius = calcPropRadius(circleValues[key]);
-
-		$('#'+key).attr({
-			cy: 59 - radius,
-			r: radius
-		});
-
-		$('#'+key+'-text').text(Math.round(circleValues[key]*100)/100);
-	};
-};
+}
 
 
-
-
-
-
-
-//FIFTH OPERATOR
-
-function getMoreData(map){
+function getPoliceData(map){
     $.ajax("../data/PoliceStations.geojson", {
         dataType: "json",
         success: function(response){
 
-        	OverlayPoliceStations(map, response);
+            OverlayPoliceStations(map, response);
         }
     });
 };
 
-function putOnMap(map, response){
-    
-
-    		var geojsonMarkerOptions = {
-                radius: 5,
-                fillColor: "#ffff00",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            policeLayer = L.geoJson(response, {
-            	pointToLayer: function(feature, latlng){
-            		return L.circleMarker(latlng, geojsonMarkerOptions);
-            	}
-            });
-            
-            policeLayer.addTo(map);
-
-            
-
-    
-};
-
-function takeOffMap(map, response){
-
-    
-
-    map.removeLayer(policeLayer);
-
-    
-};
 
 function OverlayPoliceStations(map, response){
-	var x = 0
+    var z = 0
 
-	$('#overlay').append('<button class="push" align="center"> </button>');
+    map.addSource("points1", {
+                "type": "geojson",
+                "data": response
+            });
 
-	$('.push').click(function(){
-		
-		if (x === 0){
-			putOnMap(map, response);
-			x = 1;
+    $('.policeButton').click(function(){
+        
+        if (z === 0){
+            
+            map.addLayer({
+                'id': 'police',
+                'type': 'circle',
+                'source': 'points1',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'paint': {
+                        'circle-radius': 3.5,
 
-		} else if (x === 1){
-			
-			takeOffMap(map, response);
-			x = 0;
-		}
+                    'circle-color': 'rgba(255,255,0,1)'
+                },
+                'source-layer': 'museum-cusco'
+            });
 
-	});
+            z = 1;
+
+        } else if (z === 1){
+            
+            map.removeLayer('police');
+            
+            z = 0;
+        }
+
+    });
 }
 
 
-
-
-
-
-
-function getEvenMoreData(map){
+function getHospitalData(map){
     $.ajax("../data/Hospitals.geojson", {
         dataType: "json",
         success: function(response){
 
-        	OverlayHospitals(map, response);
+            OverlayHospitals(map, response);
         }
     });
 };
 
-function putOnMap1(map, response){
-    
-
-    		var geojsonMarkerOptions = {
-                radius: 7,
-                fillColor: "#0099ff",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            HospitalsLayer = L.geoJson(response, {
-            	pointToLayer: function(feature, latlng){
-            		return L.circleMarker(latlng, geojsonMarkerOptions);
-            	}
-            });
-            
-            HospitalsLayer.addTo(map);
-
-            
-
-    
-};
-
-function takeOffMap1(map, response){
-
-    
-
-    map.removeLayer(HospitalsLayer);
-
-    
-};
 
 function OverlayHospitals(map, response){
-	var x = 0
+    var z = 0
 
-	$('#overlay2').append('<button class="push2" align="center"> </button>');
+    map.addSource("points2", {
+                "type": "geojson",
+                "data": response
+            });
 
-	$('.push2').click(function(){
-		
-		if (x === 0){
-			putOnMap1(map, response);
-			x = 1;
 
-		} else if (x === 1){
-			
-			takeOffMap1(map, response);
-			x = 0;
-		}
+    $('.hospitalButton').click(function(){
+        
+        if (z === 0){
+            
+            map.addLayer({
+                'id': 'hospital',
+                'type': 'circle',
+                'source': 'points2',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'paint': {
+                    'circle-radius': 7.5,
 
-	});
+                    'circle-color': 'rgba(109,109,109,1)'
+                },
+                'source-layer': 'museum-cusco'
+            });
+
+            z = 1;
+
+        } else if (z === 1){
+            
+            map.removeLayer('hospital');
+
+            z = 0;
+        }
+
+    });
 }
-
-
-$(document).ready(createMap);
